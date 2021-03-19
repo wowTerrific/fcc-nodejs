51985b2ee561176a3490a5c8ec21b0b6423c23fe
wowTerrific <marshallstutzman@gmail.com>
2021-03-19 11:52:08 -0400

    more

--- examples/serving-static-files.js
+++ examples/serving-static-files.js
@@ -7,6 +7,14 @@ http.createServer( (req, res) => {
 	// header - let client know what data is coming (status code, Content-type object)
 	res.writeHead(200, {'Content-type': 'image/jpg'});
 	
+	
+	// other files 
+	//const readStream = fs.createReadStream('./static/example.json');
+	//res.writeHead(200, {'Content-type': 'application/json'});
+	
+	//const readStream = fs.createReadStream('./static/index.html');
+	//res.writeHead(200, {'Content-type': 'text/html'});
+	
 	readStream.pipe(res);
 	
 }).listen(3000);
